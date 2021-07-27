81d6351685cf0ab7ce8da412a68ce61f82d6607f
Giovani Trevisol <giovanitrevisol93@gmail.com>
2021-07-26 23:10:07 -0300

    criando novo projeto

---
+++ back/plts-microservice/plts-post/src/main/java/br/com/gt/pltspost/PltsPostApplication.java
@@ -0,0 +1,13 @@
+package br.com.gt.pltspost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PltsPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PltsPostApplication.class, args);
+	}
+
+}
